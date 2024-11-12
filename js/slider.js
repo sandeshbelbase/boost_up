c03b07ab48474792d2959954f6b8267cfd66fd2e
sandesh <sandeshbelbase11@gmail.com>
2024-11-13 09:51:33 +1100

    hello

--- js/slider.js
+++ js/slider.js
@@ -19,5 +19,33 @@ $(document).ready(function () {
     infinite: true, // Enable infinite looping
     slidesToShow: 3, // Number of slides to show
     slidesToScroll: 1, // Number of slides to scroll
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
   });
 });
